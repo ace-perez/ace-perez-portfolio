30393ba4a37e8fe1a28461f888fc6e253bbf47d7
Ace Perez <acenathen.perez@gmail.com>
2025-07-28 14:32:49 +0100

    Update redeploy-site.sh
    fixed pulling from main in the script

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -7,8 +7,8 @@ LOGFILE="$LOGDIR/$(date +'%Y-%m-%d_%H-%M-%S').log"
 echo "Going to project directory..."
 cd ~/flask-portfolio || exit 1
 
-# echo "Pulling latest code from GitHub..."
-# git fetch && git reset origin/main --hard
+echo "Pulling latest code from GitHub..."
+git fetch && git reset origin/main --hard
 
 echo "Bringing down existing containers..."
 docker compose -f docker-compose.prod.yml down 2>> "$LOGFILE"
